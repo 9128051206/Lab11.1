0287545313e550f51a59e17d0782b4bd3ae2ed9b
Юлия Шепелева <9128051206@mail.ru>
2022-04-25 00:03:45 +0500

    Добавьте файлы проекта.

---
+++ Lab11.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите k: ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Solve solve = new Solve();
+            solve.root();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab11.1/Solve.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11._1
+{
+    public struct Solve
+    {
+        public double k;
+        public double b;
+        public double x;
+        public Solve(double k, double b, double x)
+        {
+            this.k = k;
+            this.b = b;
+            this.x = x;
+        }
+        public double root()
+        {
+            x = -b / k;
+            Console.WriteLine("Ответ: {0}", x);
+            return x;
+        }
+    }
+}
